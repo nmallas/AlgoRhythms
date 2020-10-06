c5d69d3f88f1f22ffe74d2f8330ce21e52b3c33c
Nick Mallas <nmallas23@gmail.com>
2020-10-06 17:57:05 -0500

    login page styled

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,12 +17,40 @@ class Login extends React.Component {
     render() {
         return (
             <div className="auth-page">
-                <form method="Post" action="/api/users/login" className="auth-form">
-                    <input type="email" name="email" value={this.state.email} onChange={this.updateInput} placeHolder="Email Address"></input>
-                    <input type="password" name="password" value={this.state.password} onChange={this.updateInput} placeHolder="Password"></input>
-                    <input type="confirmPassword" name="confirmPassword" value={this.state.confirmPassword} onChange={this.updateInput} placeHolder="Confirm Password"></input>
-                    <button type="submit"> Login</button>
-                </form>
+                <div className="auth-form-container">
+                    <div className="auth-header"> Sign In </div>
+                    <form method="Post" action="/api/users/login" className="auth-form" >
+                        <input
+                            autoComplete="off"
+                            type="email"
+                            name="email"
+                            className="auth-form-input"
+                            value={this.state.email}
+                            onChange={this.updateInput}
+                            placeHolder="Email Address">
+                        </input>
+                        <input
+                            type="password"
+                            name="password"
+                            className="auth-form-input"
+                            autoComplete="off"
+                            value={this.state.password}
+                            onChange={this.updateInput}
+                            placeHolder="Password">
+                        </input>
+                        <input
+                            type="confirmPassword"
+                            name="confirmPassword"
+                            autoComplete="off"
+                            className="auth-form-input"
+                            value={this.state.confirmPassword}
+                            onChange={this.updateInput}
+                            placeHolder="Confirm Password">
+                        </input>
+                        <button type="submit" className="auth-form-input" id="login"> Login</button>
+                        <button type="button" className="auth-form-input" id="demo"> Login as Demo User</button>
+                    </form>
+                </div>
             </div>
         )
     }
