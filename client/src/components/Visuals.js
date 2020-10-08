dc97c94319109daa0da77dacab238548ff793514
Nick Mallas <nmallas23@gmail.com>
2020-10-07 19:55:13 -0500

    finish insertion sort

--- client/src/components/Visuals.js
+++ client/src/components/Visuals.js
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
-import {BarChart, CartesianGrid, XAxis, YAxis, Bar, Cell} from "recharts";
+import {BarChart, CartesianGrid, XAxis, YAxis, Bar, Cell, Tooltip} from "recharts";
 
 
 
 export default function Visuals(props) {
-    const arr = [8,3,6,12,2,9,1]
+    const arr = [8,5,6,12,2,9,1,4]
     const [data, setData] = useState(arr.map((el, i) => ({index: i, val: el})))
 
     const updateData = (arrCopy, k, c) => setData(arrCopy.map((el, i) => ({index: i, val: el, current: k, completed: c})));
-
-
-    // const renderCurrent = (arrCopy) => {
-    //     console.log(arrCopy, n)
-    //     setTimeout(function updateGraph(){
-    //         updateData(arrCopy)
-    //     }, (1000 * n))
-    // }
 
     function sleep (time) {
         return new Promise((resolve) => setTimeout(resolve, time));
@@ -63,27 +55,49 @@ export default function Visuals(props) {
         return arrCopy
     }
 
+    const insertionSort = async(arr) => {
+        let arrCopy = [...arr];
+        let current = [0];
+        for(let i=1; i<arrCopy.length; i++) {
+            for(let j=i; j>0; j--) {
+                if(arrCopy[j] < arrCopy[j-1] ) {
+                    [arrCopy[j], arrCopy[j-1]] = [arrCopy[j-1], arrCopy[j]];
+                    updateData(arrCopy, current, [j-1]);
+                    await sleep(300 * j);
+                }
+
+            }
+            current.push(i);
+        }
+        current.push(arr.length-1)
+        updateData(arrCopy, current, []);
+        return arrCopy;
+    }
+
     const handleSort = (e) => {
         e.preventDefault();
         if(e.target.id === "bubble") {
             bubbleSort(arr);
         } else if ((e.target.id === "selection")) {
             selectionSort(arr);
+        } else {
+            insertionSort(arr);
         }
     }
 
     return (
-        <div >
+        <div className="visual-container">
             <button id="bubble" onClick={handleSort}> BubbleSort</button>
             <button id="selection" onClick={handleSort}> SelectionSort</button>
+            <button id="insertion" onClick={handleSort}> InsertionSort</button>
             <BarChart width={730} height={250} data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 {/* <XAxis dataKey="index" /> */}
                 <YAxis />
+                {/* <Tooltip position={{"x": 25, "y": 0}} animationDuration={2500}/> */}
                 <Bar dataKey="val" >
                 {
                     data.map((entry, i) => {
-                        console.log(i, entry.current)
                         const color = (entry.completed?.includes(i)) ? "#ed6663" :
                                       (entry.current?.includes(i)) ? "#ffa372":
                                       "#4e89ae";
