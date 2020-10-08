7fed8e6ad7d79f60028b1eaf65004529350df5ef
Nick Mallas <nmallas23@gmail.com>
2020-10-08 17:55:21 -0500

    quiz data displaying on page

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,22 +1,50 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 export default function Quiz(props) {
 
-    let quizId = props.match.params.quizId
+    let quizId = props.match.params.quizId;
+    let [quizData, setQuizData] = useState("");
+    let [loading, setLoading] = useState(true);
+    let [quizName, setQuizName] = useState("")
 
     useEffect(()=> {
         async function getQuiz() {
             let res = await fetch(`/api/quizzes/${quizId}`);
             if(res.ok) {
                 let data = await res.json();
-                console.log(data);
+                let name = Object.keys(data)[0]
+                setQuizName(name);
+                setQuizData(data[name]);
+                setLoading(false);
 
             }
         }
         getQuiz();
-    }, [])
+    }, [quizId])
 
-    return (
-    <h1> Quiz {quizId} </h1>
+    console.log(quizName, quizData)
+    return loading ? null : (
+        <div className="quiz-container">
+            <h1> {quizName} </h1>
+            <form>
+                {quizData.map(q => {
+                    if(q.question.type === "mc") {
+                        return (
+                            <div className="mc-question">
+                                <div className="mc-question-title"> {q.question.content} </div>
+                                <div className="mc-answers">
+                                    {
+                                    q.answers.map(a =>
+                                        <div className="mc-answer">
+                                            <input type="radio" value={a.order} />
+                                            <div >{a.content} </div>
+                                        </div>)}
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+            </form>
+        </div>
     )
 }
--- starter_app/app/api/quiz_routes.py
+++ starter_app/app/api/quiz_routes.py
@@ -25,7 +25,7 @@ def get_one(id):
     for question in questions:
         question_info = {"type": question.questionType, "answer": question.answer, "content": question.content}
         answers = AnswerChoice.query.filter(AnswerChoice.questionId == question.id).all()
-        question_and_answers = {"question": question_info, "answers": [{answer.order: answer.content} for answer in answers]}
+        question_and_answers = {"question": question_info, "answers": [{"order": answer.order, "content": answer.content} for answer in answers]}
         print(question_and_answers)
         all_questions.append(question_and_answers)
     return {quiz.name: all_questions}
