f4363df2697c95b128ef0884cdc6f10687caac9d
Nick Mallas <nmallas23@gmail.com>
2020-10-12 11:45:26 -0500

    Can add answer choices to quiz

--- client/src/components/CreateQuiz.js
+++ client/src/components/CreateQuiz.js
@@ -3,7 +3,8 @@ import React, { useState } from "react";
 export default function CreateQuiz() {
     let [quiz, setQuiz] = useState();
     let [questions, setQuestions] = useState([""]);
-    let [answerChoices, setAnswerChoices] = useState({})
+    let [answerChoices, setAnswerChoices] = useState({0: []});
+    let [currentAnswerChoice, setCurrentAnswerChoice] = useState("");
 
     const updateInput = (e) => {
         let updatedQuestions = questions.slice(0);
@@ -11,18 +12,38 @@ export default function CreateQuiz() {
         setQuestions(updatedQuestions);
     }
 
+    const addAnswerChoice = (i) => {
+        let oldAnswerChoices = {...answerChoices};
+        oldAnswerChoices[i].push(currentAnswerChoice);
+        setAnswerChoices(oldAnswerChoices);
+        setCurrentAnswerChoice("");
+    }
+
     console.log(questions);
+    console.log(currentAnswerChoice);
     return (
         <form>
             {questions.map((q, i) => {
                 return (
-                    <div className="create-quiz-question-container">
-                        <div className="create-quiz-question-label"> Question {i + 1} </div>
-                        <input  type="textArea" className="create-quiz-question"
-                                placeholder={"write question here"} value={useState[i]}
-                                name={i} onChange={updateInput}/>
-                    </div>
-                )
+                    <>
+                        <div className="create-quiz-question-container">
+                            <div className="create-quiz-question-label"> Question {i + 1} </div>
+                            <textarea  className="create-quiz-question"
+                                    placeholder={"write question here"} value={useState[i]}
+                                    name={i} onChange={updateInput}/>
+                        </div>
+                        <ol className="answer-choice-list">
+                            {answerChoices[i].map((ac, j)=> (
+                                <li> {ac} </li>
+                            ))}
+                            <li>
+                                <input onChange={(e) => setCurrentAnswerChoice(e.target.value)} value={currentAnswerChoice}/>
+                                <button type="button" onClick={() => addAnswerChoice(i)}> Add Answer Choice</button>
+                            </li>
+                        </ol>
+
+                    </>
+                    )
             })}
         </form>
     )
